3f575454bae52a3f904bea89c3a57519fd16d8d6
Kennan Gumbs <kfg021@gmail.com>
2025-08-04 03:44:24 -0400

    change type in hand evaluator

--- include/game/holdem/hand_evaluator.hpp
+++ include/game/holdem/hand_evaluator.hpp
@@ -11,7 +11,7 @@ public:
     ShowdownResult getShowdownResult(const std::array<CardSet, 2>& playerHands, CardSet board) const;
 
 private:
-    constexpr static int HandRankTableSize = 2598960; // 52 choose 5
+    static constexpr std::uint32_t HandRankTableSize = 2598960; // 52 choose 5
     using ChooseTable = std::array<std::array<std::uint32_t, 5>, 52>;
     using HandRankTable = std::array<std::uint32_t, HandRankTableSize>;
 
