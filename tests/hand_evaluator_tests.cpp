49157f588003b2ac1312703e17aa5f008fb42982
Kennan Gumbs <kfg021@gmail.com>
2025-08-04 23:06:39 -0400

    create hand evaluator tests

---
+++ tests/hand_evaluator_tests.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+
+#include "game/game_types.hpp"
+#include "game/game_utils.hpp"
+#include "game/holdem/hand_evaluator.hpp"
+
+#include <array>
+#include <cstdint>
+#include <unordered_map>
+
+TEST(HandEvaluatorTest, CorrectNumberOfEachHandType) {
+    static constexpr int NumHandRankings = 10;
+    std::array<std::uint32_t, NumHandRankings> ExpectedTotalHandsPerRank = {
+        1302540,    // High Card
+        1098240,    // One Pair
+        123552,     // Two Pair
+        54912,      // Three of a Kind
+        10200,      // Straight
+        5108,       // Flush
+        3744,       // Full House
+        624,        // Four of a Kind
+        36,         // Straight Flush
+        4,          // Royal Flush
+    };
+
+    HandEvaluator handEvaluator; 
+    std::array<std::uint32_t, NumHandRankings> totalHandsPerRank = {};
+    for (CardID card0 = 0; card0 < 52; ++card0) {
+        for (CardID card1 = card0 + 1; card1 < 52; ++card1) {
+            for (CardID card2 = card1 + 1; card2 < 52; ++card2) {
+                for (CardID card3 = card2 + 1; card3 < 52; ++card3) {
+                    for (CardID card4 = card3 + 1; card4 < 52; ++card4) {
+                        CardSet hand = cardIDToSet(card0) |
+                            cardIDToSet(card1) |
+                            cardIDToSet(card2) |
+                            cardIDToSet(card3) |
+                            cardIDToSet(card4);
+
+                        std::uint32_t handRank = handEvaluator.getFiveCardHandRank(hand);
+                        std::uint8_t handType = (handRank >> 20) & 0xF;
+                        EXPECT_LT(handType, NumHandRankings);
+                        ++totalHandsPerRank[handType];
+                    }
+                }
+            }
+        }
+    }
+
+    EXPECT_EQ(totalHandsPerRank, ExpectedTotalHandsPerRank);
+}
